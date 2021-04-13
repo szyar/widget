3c1fcdfb97c7d0ad47c9bf9a76154631b4b04a8d
szyar <soezayarszyar@gmail.com>
2021-04-13 20:40:40 +0630

    starting accordion component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import Accordion from './components/Accordion';
+
+const items = [
+  {
+    title: 'What is React?',
+    content: 'React is front-end JavaScript framework'
+  },
+  {
+    title: 'Why use React?',
+    content: 'React is favorite JS library among engineers'
+  },
+  {
+    title: 'How do you use React',
+    content: 'You use React by creating components'
+  }
+];
 
 const App = () => {
   return (
     <div>
-      Hello from App
+      <Accordion items={items} />
     </div>
   );
 };
